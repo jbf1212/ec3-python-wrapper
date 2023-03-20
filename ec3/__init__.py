8208599ea6aea3eaf050a9ecfa62f28d7f8e204b
Jared Friedman <jbf1212@gmail.com>
2023-03-20 03:22:04 -0400

    version bump 0.0.8

--- ec3/__init__.py
+++ ec3/__init__.py
@@ -1,6 +1,7 @@
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from .ec3_materials import EC3Materials
 from .ec3_epds import EC3epds
 from .ec3_urls import EC3URLs
 from .ec3_projects import EC3Projects
+from .ec3_categories import EC3Categories
