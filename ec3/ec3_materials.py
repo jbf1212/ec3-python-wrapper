9a8c86552968682d24eac4998c88a6d4f4922ad0
jbf1212 <jbf1212@gmail.com>
2024-09-16 01:28:47 -0400

    updates for materialfilter query api changes

--- ec3/ec3_materials.py
+++ ec3/ec3_materials.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 from .ec3_api import EC3Abstract
 from .ec3_urls import EC3URLs
@@ -41,15 +42,12 @@ class EC3Materials(EC3Abstract):
             fields_string = ",".join(self.return_fields)
             params["params"]["fields"] = fields_string
 
+            # FIXME seems like this is not return full dictionaries for nested values
+
         # NOTE "sort_by" is not currently working as expected when passing multiple fields.
         # Setting up to expect a single string field temporarily.
         if self.sort_by:
             params["params"]["sort_by"] = self.sort_by
-
-        if self.only_valid:
-            params["params"]["epd__date_validity_ends__gt"] = datetime.today().strftime(
-                "%Y-%m-%d"
-            )
 
         if self.masterformat_filter:
             ec3_categories = EC3Categories(
@@ -73,6 +71,88 @@ class EC3Materials(EC3Abstract):
         Returns:
             list: List of dictionaries of matching material records
         """
+        if self.only_valid:
+            params["params"]["epd__date_validity_ends__gt"] = datetime.today().strftime(
+                "%Y-%m-%d"
+            )
+
+        processed_params = self._process_params(params)
+
+        if return_all:
+            return super()._get_all(self.url.materials_url(), **processed_params)
+        else:
+            return super()._get_records(self.url.materials_url(), **processed_params)
+
+    def convert_query_to_mf_string(self, category_name, field_dict_list, pragma=None):
+        """
+        Converts a dictionary of material search parameters to a pragma string for use in the EC3 API
+        This function includes a POST request that requires an API key with write access
+
+        Args:
+            category_name (str): EC3 category name (see https://docs.open-epd-forum.org/en/data-format/materials/ for list of valid category names)
+            field_dict_list (list): List of dictionaries of search parameters (format: [{"field": "field_name", "op": "operator", "arg": "argument"}])
+            pragma (list, optional): List of dictionaries of pragma parameters. Defaults to eMF 2.0/1 and TRACI 2.1.
+
+        Returns:
+            str: string formatted to work with MaterialFilter pragma in EC3 API
+        """
+        payload_dict = {}
+
+        if pragma is None:
+            pragma = [
+                {"name": "eMF", "args": ["2.0/1"]},
+                {"name": "lcia", "args": ["TRACI 2.1"]},
+            ]
+
+        payload_dict["pragma"] = pragma
+        payload_dict["category"] = category_name
+        payload_dict["filter"] = field_dict_list
+
+        payload = json.dumps(payload_dict)
+
+        mf_url = (
+            self.url.materials_convert_matfilter_url()
+            + "?output=string&output_style=compact"
+        )
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.bearer_token}",
+        }
+        response = self.session.request(
+            "post", mf_url, verify=self._ssl_verify, data=payload, headers=headers
+        )
+
+        return response
+
+    def get_materials_mf(self, category_name, mf_list, return_all=False, **params):
+        """
+        Returns matching materials using filters
+
+        Args:
+            category_name (str): Open EPD category name (see https://docs.open-epd-forum.org/en/data-format/materials/ for list of valid category names)
+            mf_list (list): List of dictionaries of search parameters (format: [{"field": "field_name", "op": "operator", "arg": "argument"}])
+            return_all (bool, optional): Set to True to return all matches. Defaults to False, which will return the quantity specified in max_records.
+
+        Returns:
+            list: List of dictionaries of matching material records
+        """
+        if self.only_valid:
+            mf_list.append(
+                {
+                    "field": "epd__date_validity_ends",
+                    "op": "gt",
+                    "arg": datetime.today().strftime("%Y-%m-%d"),
+                }
+            )
+
+        mf_response = self.convert_query_to_mf_string(category_name, mf_list)
+        mf_response_json = mf_response.json()
+        mf_string = mf_response_json["material_filter_str"]
+
+        params["params"] = {}
+        params["params"]["mf"] = mf_string
+
         processed_params = self._process_params(params)
 
         if return_all:
@@ -107,6 +187,45 @@ class EC3Materials(EC3Abstract):
         params["params"]["plant__distance__lt"] = plant_distance
 
         return self.get_materials(return_all=return_all, **params)
+
+    def get_materials_within_region_mf(
+        self,
+        category_name,
+        mf_list,
+        postal_code,
+        country_code="US",
+        plant_distance="100 mi",
+        return_all=False,
+        **params,
+    ):
+        """
+        Returns only materials from plants within provided distance of postal code.
+        This adds the "latitude", "longitude", and "plant_distance_lt" keys to your parameter dictionary.
+
+        Args:
+            category_name (str): Open EPD category name (see https://docs.open-epd-forum.org/en/data-format/materials/ for list of valid category names)
+            mf_list (list): List of dictionaries of search parameters (format: [{"field": "field_name", "op": "operator", "arg": "argument"}])
+            postal_code (int): postal code
+            country_code (str, optional): Two letter country code.. Defaults to 'US'.
+            plant_distance (str, optional): Distance to plant with units in string ('mi' or 'km'). Defaults to "100 mi".
+            return_all (bool, optional): Set to True to return all matches. Defaults to False, which will return the quantity specified in max_records.
+
+        Returns:
+            list: List of dictionaries of matching material records within distance provided from postal code
+        """
+        lat, long = postal_to_latlong(postal_code, country_code)
+
+        mf_list.extend(
+            [
+                {"field": "latitude", "op": "exact", "arg": lat},
+                {"field": "longitude", "op": "exact", "arg": long},
+                {"field": "plant__distance", "op": "lt", "arg": plant_distance},
+            ]
+        )
+
+        return self.get_materials_mf(
+            category_name, mf_list, return_all=return_all, **params
+        )
 
     # NOTE Querying materials by "open_xpd_uuid" does not appear to currently work with the api
     # def get_material_by_xpduuid(self, epd_xpd_uuid):
